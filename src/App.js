5d1b77fd5a73a2a299c788514858bc27b45b62d4
Luiz Guilherme Guizi <guilhermeluizguizi@outlook.com>
2025-06-04 21:57:56 -0300

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    setLoading(true)
+    axios.get("https://api.jolpi.ca/ergast/f1/current/driverStandings.json")
+    .then((response) =>{
+      const driverList = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+      setDrivers(driverList)
+      setLoading(false)
+    })
+    .catch((error) => {
+      console.log("Erro na requisição", error)
+      setError("Erro ao carregar os dados. Tente novamente.")
+      setLoading(false)
+    })
+  },[])
+
+  const [drivers, setDrivers] = useState([]);
+  const [loading, setLoading] =  useState(true)
+  const [error, setError] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Classificação Fórmula 1 2025</h1>
+      {loading && <p>Carregando Dados...</p>}
+      {error && <p style={{color:'red'}}>{error}</p>}
+      {!loading && !error && (
+        <ul>
+        {drivers.map((driver) => (
+    
+       <li
+        key={driver.Driver.driverId}
         >
-          Learn React
-        </a>
-      </header>
+          {driver.position}º - {driver.Driver.givenName} {driver.Driver.familyName} ({driver.Constructors[0].name}) - {driver.points} pts
+        </li>
+      ))}
+      </ul>
+      )}
     </div>
   );
 }
-
 export default App;
